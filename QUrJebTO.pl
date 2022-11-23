aae4de948f025c6d452ee078d4c1f45c53f86eb0
Leonardo Ramos <101880063+leonardopramos@users.noreply.github.com>
2022-11-23 16:23:02 -0300

    Add files via upload

---
+++ QUrJebTO.pl
@@ -0,0 +1,195 @@
+//seleções: nome,pontos,gols feitos, gols tomados.
+brasil("brasil", 3, 3, 1).
+argentina("argentina", 0, 2, 1).
+espanha("espanha", 3, 7, 0).
+alemanha("alemanha", 0, 1, 2).
+portugal("portugal", 0, 2, 0).
+frança("frança", 3, 4, 1).
+
+//tecnico: nome, seleção
+tecnico("tite", "Brasil").
+//jogador: nome, numero, posiçao, titular/reserva, selecao.
+jogador("alisson", 1, "goleiro", "titular", "brasil").
+jogador("danilo", 2, "lateral", "titular", "brasil").
+jogador("thiago Silva", 3, "zagueiro", "titular", "brasil").
+jogador("marquinhos", 4, "zagueiro", "titular", "brasil").
+jogador("casemiro", 5, "volante", "titular", "brasil").
+jogador("alex Sandro", 6, "lateral", "titular", "brasil").
+jogador("paquetá", 7, "meio campista", "titular", "brasil").
+jogador("fred", 8, "volante", "banco", "brasil").
+jogador("richarlison", 9, "atacante", "titular", "brasil").
+jogador("neymar Jr", 10, "atacante", "titular", "brasil").
+jogador("raphinha", 11, "atacante", "titular", "brasil").
+jogador("weverton", 12, "goleiro", "banco", "brasil").
+jogador("dani Alves", 13, "lateral", "banco", "brasil").
+jogador("e. Militão", 14, "zagueiro", "banco", "brasil").
+jogador("fabinho", 15, "volante", "banco", "brasil").
+jogador("alex Telles", 16, "lateral", "banco", "brasil").
+jogador("bruno Guimarães", 17, "volante", "banco", "brasil").
+jogador("gabriel Jesus", 18, "atacante", "banco", "brasil").
+jogador("antony", 19, "atacante", "banco", "brasil").
+jogador("vinicius Junior", 20, "atacante", "titular", "brasil").
+jogador("rodrygo", 21, "atacante", "banco", "brasil").
+jogador("everton Ribeiro", 22, "meio campista", "banco", "brasil").
+jogador("ederson", 23, "goleiro", "banco", "brasil").
+jogador("bremer", 24, "zagueiro", "banco", "brasil").
+jogador("pedro", 25, "atacante", "banco", "brasil").
+jogador("g.Martinelli", 26, "atacante", "banco", "brasil").
+
+//tecnico: nome, seleção
+tecnico("lionel scaloni", "argentina").
+//jogador: nome, numero, posiçao, titular/reserva, selecao.
+jogador("franco armani", 1, "goleiro", "banco", "argentina").
+jogador("juan foyth", 2, "lateral", "banco", "argentina").
+jogador("nicolás tagliafico", 3, "lateral", "titular", "argentina").
+jogador("gonzalo montiel", 4, "zagueiro", "banco", "argentina").
+jogador("leandro paredes", 5, "volante", "titular", "argentina").
+jogador("gastón pezella", 6, "zagueiro", "banco", "argentina").
+jogador("rodrigo de paul", 7, "meio campista", "titular", "argentina").
+jogador("marcos acuña", 8, "volante", "banco", "argentina").
+jogador("julian alvarez", 9, "atacante", "banco", "argentina").
+jogador("messi", 10, "atacante", "titular", "argentina").
+jogador("di maria", 11, "meio campista", "titular", "argentina").
+jogador("gerónimo rulli", 12, "goleiro", "banco", "argentina").
+jogador("cristiano romero", 13, "lateral", "titular", "argentina").
+jogador("exequiel palacios", 14, "meio campista", "banco", "argentina").
+jogador("nicolás gonzales", 15, "volante", "banco", "argentina").
+jogador("joaquín correa", 16, "atacante", "banco", "argentina").
+jogador("alejandro gomez", 17, "volante", "titular", "argentina").
+jogador("guido rodríguez", 18, "meio campista", "banco", "argentina").
+jogador("nicolás otamentid", 19, "atacante", "titular", "argentina").
+jogador("alexis mac allister", 20, "meio campista", "banco", "argentina").
+jogador("paulo dybala", 21, "atacante", "banco", "argentina").
+jogador("lautaro martinez", 22, "atacante", "titular", "argentina").
+jogador("emiliano martinez", 23, "goleiro", "titular", "argentina").
+jogador("enzo fernandez", 24, "meio campista", "banco", "argentina").
+jogador("lisandro martinez", 25, "atacante", "banco", "argentina").
+jogador("nahuel molina", 26, "zagueiro", "titular", "argentina").
+
+
+//tecnico: nome, seleção
+tecnico("luis enrique", "espanha").
+//jogador: nome, numero, posiçao, titular/reserva, selecao.
+jogador("robert sanchez", 1, "goleiro", "banco", "espanha").
+jogador("azpilicueta", 2, "lateral", "titular", "espanha").
+jogador("eric garcia", 3, "zagueiro", "banco", "espanha").
+jogador("pau torres", 4, "zagueiro", "banco", "espanha").
+jogador("sergio busquets", 5, "volante", "titular", "espanha").
+jogador("llorente", 6, "meio campista", "banco", "espanha").
+jogador("morata", 7, "atacante", "banco", "espanha").
+jogador("koke", 8, "meio campista", "banco", "espanha").
+jogador("gavi", 9, "meio campista", "titular", "espanha").
+jogador("asensio", 10, "meio campista", "titular", "espanha").
+jogador("ferran torres", 11, "atacante", "titular", "espanha").
+jogador("williams", 12, "atacante", "banco", "espanha").
+jogador("david raya", 13, "goleiro", "banco", "espanha").
+jogador("alejandro balde", 14, "lateral", "banco", "espanha").
+jogador("guillamón", 15, "volante", "banco", "espanha").
+jogador("rodri", 16, "volante", "titular", "espanha").
+jogador("pino", 17, "atacante", "banco", "espanha").
+jogador("jordi alba", 18, "lateral", "titular", "espanha").
+jogador("carlos soler", 19, "atacante", "banco", "espanha").
+jogador("carvajal", 20, "lateral", "banco", "espanha").
+jogador("dani olmo", 21, "atacante", "titular", "espanha").
+jogador("pablo sarabia", 22, "meio campista", "banco", "espanha").
+jogador("unai simón", 23, "goleiro", "titular", "espanha").
+jogador("laporte", 24, "zagueiro", "titular", "espanha").
+jogador("ansu fati", 25, "atacante", "banco", "espanha").
+jogador("pedri", 26, "meio campista", "titular", "espanha").
+
+//tecnico: nome, seleção
+tecnico("hans-dieter flick", "alemanha").
+//jogador: nome, numero, posiçao, titular/reserva, selecao.
+jogador("manuel neuer", 1, "goleiro", "titular", "alemanha").
+jogador("rudiger", 2, "zagueiro", "titular", "alemanha").
+jogador("raum", 3, "lateral", "titular", "alemanha").
+jogador("ginter", 4, "zagueiro", "banco", "alemanha").
+jogador("kehrer", 5, "zagueiro", "banco", "alemanha").
+jogador("kimmich", 6, "volante", "titular", "alemanha").
+jogador("havertz", 7, "meio campista", "titular", "alemanha").
+jogador("gortezka", 8, "volante", "banco", "alemanha").
+jogador("fullkrug", 9, "atacante", "banco", "alemanha").
+jogador("gnabry", 10, "atacante", "titular", "alemanha").
+jogador("gotze", 11, "meio campista", "banco", "alemanha").
+jogador("trapp", 12, "goleiro", "banco", "alemanha").
+jogador("muller", 13, "atacante", "titular", "alemanha").
+jogador("musiala", 14, "meio campista", "titular", "alemanha").
+jogador("sule", 15, "zagueiro", "titular", "alemanha").
+jogador("klostermann", 16, "lateral", "banco", "alemanha").
+jogador("brandtt", 17, "meio campista", "banco", "alemanha").
+jogador("hofmann", 18, "atacante", "banco", "alemanha").
+jogador("sané", 19, "atacante", "banco", "alemanha").
+jogador("gunter", 20, "lateral", "banco", "alemanha").
+jogador("gundogan", 21, "meio campista", "titular", "alemanha").
+jogador("ter stegen", 22, "goleiro", "banco", "alemanha").
+jogador("schlotterbeck", 23, "zagueiro", "titular", "alemanha").
+jogador("adeyemi", 24, "atacante", "banco", "alemanha").
+jogador("bella-kotchap", 25, "zagueiro", "banco", "alemanha").
+jogador("moukoko", 26, "atacante", "banco", "alemanha").
+
+//tecnico: nome, seleção
+tecnico("fernando santos", "portugal").
+//jogador: nome, numero, posiçao, titular/reserva, selecao.
+jogador("rui patricio", 1, "goleiro", "banco", "portugal").
+jogador("dalot", 2, "lateral", "banco", "portugal").
+jogador("pepe", 3, "zagueiro", "banco", "portugal").
+jogador("ruben dias", 4, "zagueiro", "titular", "portugal").
+jogador("raphael guerreiro", 5, "lateral", "titular", "portugal").
+jogador("palhinha", 6, "volante", "banco", "portugal").
+jogador("cristiano ronaldo", 7, "atacante", "titular", "portugal").
+jogador("bruno fernandes", 8, "volante", "titular", "portugal").
+jogador("andré silva", 9, "atacante", "banco", "portugal").
+jogador("bernardo silva", 10, "meio campista", "titular", "portugal").
+jogador("joao felix", 11, "atacante", "banco", "portugal").
+jogador("jorge sá", 12, "goleiro", "banco", "portugal").
+jogador("danilo pereira", 13, "volante", "titular", "portugal").
+jogador("william carvalho", 14, "volante", "titular", "portugal").
+jogador("rafael leao", 15, "atacante", "titular", "portugal").
+jogador("vitinha", 16, "meio campista", "banco", "portugal").
+jogador("joão mario", 17, "meio campista", "banco", "portugal").
+jogador("ruben neves", 18, "volante", "titular", "portugal").
+jogador("nuno mendes", 19, "lateral", "", "portugal").
+jogador("joao cancelo", 20, "lateral", "titular", "portugal").
+jogador("ricardo horta", 21, "atacante", "banco", "portugal").
+jogador("diogo costa", 22, "goleiro", "titular", "portugal").
+jogador("matheus nunes", 23, "meio campista", "banco", "portugal").
+jogador("antónio silva", 24, "zagueiro", "banco", "portugal").
+jogador("otavio", 25, "atacante", "", "portugal").
+jogador("gonçalo ramos", 26, "atacante", "banco", "portugal").
+
+//tecnico: nome, seleção
+tecnico("deschamps", "frança").
+//jogador: nome, numero, posiçao, titular/reserva, selecao.
+jogador("hugo lloris", 1, "goleiro", "titular", "frança").
+jogador("pavard", 2, "lateral", "titular", "frança").
+jogador("diasi", 3, "zagueiro", "banco", "frança").
+jogador("varane", 4, "zagueiro", "banco", "frança").
+jogador("koundé", 5, "zagueiro", "banco", "frança").
+jogador("guendouzi", 6, "meio campista", "banco", "frança").
+jogador("griezmann", 7, "atacante", "titular", "frança").
+jogador("tchouameni", 8, "volante", "titular", "frança").
+jogador("giroud", 9, "atacante", "titular", "frança").
+jogador("mbappe", 10, "atacante", "titular", "frança").
+jogador("dembele", 11, "atacante", "titular", "frança").
+jogador("kolo muani", 12, "atacante", "banco", "frança").
+jogador("fofana", 13, "meio campista", "banco", "frança").
+jogador("rabiot", 14, "meio campista", "titular", "frança").
+jogador("veretout", 15, "volante", "banco", "frança").
+jogador("mandanda", 16, "goleiro", "banco", "frança").
+jogador("saliba", 17, "zagueiro", "banco", "frança").
+jogador("upamecano", 18, "zagueiro", "titular", "frança").
+jogador("coman", 20, "atacante", "banco", "frança").
+jogador("lucas hernandez", 21, "zagueiro", "titular", "frança").
+jogador("theo hernández", 22, "lateral", "banco", "frança").
+jogador("areola", 23, "goleiro", "banco", "frança").
+jogador("konaté", 24, "zagueiro", "titular", "frança").
+jogador("camavinga", 25, "meio campista", "banco", "frança").
+jogador("marcus thuram", 26, "atacante", "banco", "frança").
+
+
+pais e sua pontuação atual
+jogadores titulares de uma equipe
+nome do técnico de uma equipe
+número de gols feitos por uma equipe
+número de gols tomados por uma equipe
+número, nome  e posição de cada jogador de uma equipe
